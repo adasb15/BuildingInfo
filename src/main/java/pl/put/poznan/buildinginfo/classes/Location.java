7a5f276a4476bf72c711d820851848e91a95029e
Agata Kashyna <agata.kashyna@gmail.com>
2024-12-01 22:55:37 +0100

    Update and rename Location.txt to Location.java

---
+++ src/main/java/pl/put/poznan/buildinginfo/classes/Location.java
@@ -0,0 +1,18 @@
+package pl.put.poznan.buildinginfo.classes;
+
+
+
+public abstract class Location {
+
+    private int id;
+    private String name;
+  
+    public Location(int id, String name) {
+          this.id = id;
+          this.name = name;
+      }
+
+  //miejsce na gettery (getID, getName)
+
+  //miejsce na settery (setID,setName)
+}
